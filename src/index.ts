9db16edac755d70b5264527410a3bb89ac5813d6
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-07-24 13:31:11 -0300

    feat: install express and set initial configuration

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,55 @@
-import { AppDataSource } from "./configs/db"
-import { User } from "./models/User"
+import express from 'express';
+import { AppDataSource } from "./configs/db";
+import { User } from "./models/User";
 import { Role } from './models/Role';
 import { Post } from './models/Post';
 import { Comment } from './models/Comments';
 
-AppDataSource.initialize().then(async () => {
+const PORT = 3333;
+const app = express();
 
-    console.log("Inserting a new user into the database...")
+app.use(express.json())
 
-    const role = new Role() 
-    role.name = "Administrator"
-    await AppDataSource.manager.save(role)
+app.listen(3333, () => {
 
-    const user = new User()
-    user.name = "Suellen Camargo"
-    user.email = "suellen_fleur@hotmail.com"
-    user.password = "123456"
-    user.gender = "Female"
-    user.ocupation = "Full Stack Developer Student"
-    user.city = "Curitiba"
-    user.role = role
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    console.log(`Server is running on port ${PORT}`)
 
-    const post = new Post()
-    post.message = "Cream Crackers: the best team of this hackathon! o/"
-    post.user = user
-    await AppDataSource.manager.save(post)
+    AppDataSource.initialize().then(async () => {
+    
+        console.log("Inserting a new user into the database...")
+    
+        const role = new Role() 
+        role.name = "Administrator"
+        await AppDataSource.manager.save(role)
+    
+        const user = new User()
+        user.name = "Suellen Camargo"
+        user.email = "suellen_fleur@hotmail.com"
+        user.password = "123456"
+        user.gender = "Female"
+        user.ocupation = "Full Stack Developer Student"
+        user.city = "Curitiba"
+        user.role = role
+        await AppDataSource.manager.save(user)
+        console.log("Saved a new user with id: " + user.id)
+    
+        const post = new Post()
+        post.message = "Cream Crackers: the best team of this hackathon! o/"
+        post.user = user
+        await AppDataSource.manager.save(post)
+    
+        const comment = new Comment()
+        comment.user = user
+        comment.post = post
+        comment.message = "Our Goal is: Equal Access To Quality Education!"
+        await AppDataSource.manager.save(comment)
+    
+        console.log("Loading users from the database...")
+        const users = await AppDataSource.manager.find(User)
+        console.log("Loaded users: ", users)
+    
+        console.log("Here you can setup and run express / fastify / any other framework.")
+    
+    }).catch(error => console.log(error))
+})
 
-    const comment = new Comment()
-    comment.user = user
-    comment.post = post
-    comment.message = "Our Goal is: Equal Access To Quality Education!"
-    await AppDataSource.manager.save(comment)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
-}).catch(error => console.log(error))
