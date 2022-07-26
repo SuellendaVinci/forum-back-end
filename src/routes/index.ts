81a17100698a173303fef4653ac91d9db537e559
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-07-26 12:58:58 -0300

    feat: implement route to create post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import rolesRoutes from './rolesRoutes';
 import userRoutes from './usersRoutes';
+import postRoutes from './postsRoutes';
 
 const routes = Router();
 
 routes.use('/roles', rolesRoutes);
 routes.use('/users', userRoutes);
+routes.use('/posts', postRoutes);
 
 export default routes;
