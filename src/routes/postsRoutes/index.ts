4ec0aed4e9efcac093122b0e67e6ef2407f68691
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-07-26 15:00:49 -0300

    fix: allow just logged user to create a post

--- src/routes/postsRoutes/index.ts
+++ src/routes/postsRoutes/index.ts
@@ -1,17 +1,27 @@
 import { Router } from 'express';
 import PostDto from '../../dtos/postDto';
 import CreatePostUseCase from '../../useCases/posts/createPost';
+import jwt from 'jsonwebtoken';
+import verifyUser from '../../services/verifyUser';
 
 const postRoutes = Router();
 
 postRoutes.post('/', async (req, res) => {
-  const { message, userId } = req.body as PostDto;
+  const { message } = req.body as PostDto;
+
+  const verifyUserResponse = await verifyUser(req.headers.authorization);
+
+  if (verifyUserResponse.statusCode === 401) {
+    const { statusCode, data } = verifyUserResponse;
+    res.status(statusCode).send(data);
+    return;
+  }
 
   const createPostUseCase = new CreatePostUseCase();
 
   const response = await createPostUseCase.execute({
     message,
-    userId,
+    userId: verifyUserResponse.data.userId,
   });
 
   const { statusCode, data } = response;
