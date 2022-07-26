8f727d9e0b5c2e35117c56fc1ad1d43db02f2908
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-07-26 13:36:04 -0300

    feat: implement login usecase with JWT token

---
+++ src/useCases/login/index.ts
@@ -0,0 +1,37 @@
+import { Repository } from 'typeorm';
+import { User } from '../../models/User';
+import UsersRepository from '../../repositories/usersRepository';
+import UserDto from '../../dtos/userDto';
+import jwt from 'jsonwebtoken';
+
+import HttpResponseDto from '../../dtos/httpResponseDto';
+
+export default class LoginUseCase {
+  private _usersRepository: Repository<User>;
+
+  constructor() {
+    this._usersRepository = UsersRepository;
+  }
+
+  public async execute({
+    email,
+    password,
+  }: Pick<UserDto, 'email' | 'password'>): Promise<HttpResponseDto | null> {
+    const user = await this._usersRepository.findOne({
+      where: { email, password },
+    });
+
+    if (!user?.id) {
+      return {
+        statusCode: 404,
+        data: { error: 'Usuário ou senha incorretos' },
+      };
+    }
+
+    const token = jwt.sign({ id: user.id }, 'forum', {
+      expiresIn: 60 * 60 * 24 * 7,
+    });
+
+    return { statusCode: 201, data: { ...user, access_token: token } };
+  }
+}
