e55c19e668ca06bcdedb8658ba35c7b02d6c8406
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-07-29 04:44:46 -0300

    feat: create update comment route

--- src/routes/commentsRoutes/index.ts
+++ src/routes/commentsRoutes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import CommentDto from '../../dtos/commentDto';
 import CreateCommentUseCase from '../../useCases/comments/createComment';
+import UpdateCommentUseCase from '../../useCases/comments/updateComment';
 import verifyUser from '../../services/verifyUser';
 
 const commentsRoutes = Router();
@@ -21,6 +22,31 @@ commentsRoutes.post('/', async (req, res) => {
   const response = await createCommentUseCase.execute({
     message,
     postId,
+    userId: verifyUserResponse.data.userId,
+  });
+
+  const { statusCode, data } = response;
+
+  res.status(statusCode).send(data);
+});
+
+commentsRoutes.put('/:id', async (req, res) => {
+  const { message } = req.body as CommentDto;
+  const { id } = req.params;
+
+  const verifyUserResponse = await verifyUser(req.headers.authorization);
+
+  if (verifyUserResponse.statusCode === 401) {
+    const { statusCode, data } = verifyUserResponse;
+    res.status(statusCode).send(data);
+    return;
+  }
+
+  const updateCommentUseCase = new UpdateCommentUseCase();
+
+  const response = await updateCommentUseCase.execute({
+    id: Number(id),
+    message,
     userId: verifyUserResponse.data.userId,
   });
 
