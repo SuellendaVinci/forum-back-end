f51abce553ec26fa52459e6487e13c2458be643c
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-07-29 17:42:43 -0300

    feat: implement delete comment route

--- src/routes/commentsRoutes/index.ts
+++ src/routes/commentsRoutes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import CommentDto from '../../dtos/commentDto';
 import CreateCommentUseCase from '../../useCases/comments/createComment';
 import UpdateCommentUseCase from '../../useCases/comments/updateComment';
+import DeleteCommentUseCase from '../../useCases/comments/deleteComment';
 import verifyUser from '../../services/verifyUser';
 
 const commentsRoutes = Router();
@@ -47,6 +48,29 @@ commentsRoutes.put('/:id', async (req, res) => {
   const response = await updateCommentUseCase.execute({
     id: Number(id),
     message,
+    userId: verifyUserResponse.data.userId,
+  });
+
+  const { statusCode, data } = response;
+
+  res.status(statusCode).send(data);
+});
+
+commentsRoutes.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const verifyUserResponse = await verifyUser(req.headers.authorization);
+
+  if (verifyUserResponse.statusCode === 401) {
+    const { statusCode, data } = verifyUserResponse;
+    res.status(statusCode).send(data);
+    return;
+  }
+
+  const deleteCommentUseCase = new DeleteCommentUseCase();
+
+  const response = await deleteCommentUseCase.execute({
+    id: Number(id),
     userId: verifyUserResponse.data.userId,
   });
 
